0dd38ab5201fd97444c5c3d8e46086b71b7f9249
Zeurg <zacharyjuergensen@gmail.com>
2025-12-08 18:51:40 -0700

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+
+        
     }
 
 
